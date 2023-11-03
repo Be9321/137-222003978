968f40af9191a858c90e6c3020016d3008b999e4
Belyse URWIDUKUNDA 137 <149575349+Be9321@users.noreply.github.com>
2023-11-03 15:06:28 +0200

    Add files via upload
    EXAMPLE OF IF STATEMENTS

---
+++ App1.java
@@ -0,0 +1,12 @@
+public class App1 {
+        public static void main(String args[]) {
+            int a = 10;
+            int b = 15;
+            if (b > a) {
+                System.out.println("A is greater than B");
+            } else {
+                System.out.println("B is greater than A");
+            }
+        }
+    }
+
---
+++ App2.java
@@ -0,0 +1,26 @@
+public class App2 {
+    public static void main (String []args ) {
+        int age = 85 ;
+        if(age<35){  
+
+        System.out.println("look black panther");  
+    }  
+    else if(age>=35 && age<45){  
+        System.out.println("look outer banks");  
+    }  
+    else if(age>=45 && age<55){  
+        System.out.println("look the last ship");  
+    }  
+    else if(age>=55 && age<65){  
+        System.out.println("look the secrets");  
+    }  
+    else if( age>=65 && age<75){  
+        System.out.println("look the serial killer");  
+    }else if(age>=75 && age<85){  
+        System.out.println("look invisible man");  
+    }else{  
+        System.out.println("go to sleep");  
+    }  
+}  
+}
+
