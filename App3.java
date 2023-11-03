3d59a7d9723af0e21dfc3934a659929ac110e9bc
Belyse URWIDUKUNDA 137 <149575349+Be9321@users.noreply.github.com>
2023-11-03 15:07:35 +0200

    Add files via upload
    EXAMPLES OF SWITCH CASE

---
+++ App3.java
@@ -0,0 +1,19 @@
+public class App3 {
+    public static void main(String[] args) {  
+        int marks=45;  
+        switch(marks){  
+        case 15: System.out.println("15 marks of Naomi");  
+        break;  
+        case 25: System.out.println("25  marks of Feza");  
+        break;  
+        case 35: System.out.println("35 marks of Prince");  
+        break;
+        case 45: System.out.println("45 marks of Boubouna");
+        break;
+ 
+        default:System.out.println("it is not 15 marks of Naomi, 25 marks of Feza, 35 marks of Prince, 45 marks of Boubouna");  
+        }  
+    }  
+    }
+    
+
