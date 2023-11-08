552e1fb145a6563ff7522e67358b1a22edeff576
Belyse URWIDUKUNDA 137 <149575349+Be9321@users.noreply.github.com>
2023-11-08 21:08:35 +0200

    1st inheritance in java

--- example1.java
+++ example1.java
@@ -1,10 +1,41 @@
-public class example1 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Happy Birthday!");
-        }
+interface MonthDescription {
+    void describeMonth();
+}
+class Month implements MonthDescription {
+    protected String name;
+    protected int days;
+
+    public Month(String name, int days) {
+        this.name = name;
+        this.days = days;
+    }
+
+    public void describeMonth() {
+        System.out.println("Month: " + name);
+        System.out.println("Number of Days: " + days);
     }
 }
-    
+class January extends Month {
+    public January() {
+        super("January", 31);
+    }
+}
+class February extends Month {
+    public February() {
+        super("February", 28);
+    }
+}
+class MonthDemo {
+    public static void main(String[] args) {
+        MonthDescription january = new January();
+        MonthDescription february = new February();
 
+        january.describeMonth();
+        february.describeMonth();
+    }
+}
+
+
+
+    
 
